8bfa1f493d7afb4e0eac17da87100db1f7d28e18
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-02 21:04:25 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,7 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if { |flavor| flavor == "strawberry" }
+  
+  contacts  
 end
-
